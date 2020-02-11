e7ae28049c457a2947238d84195df2e7185a6633
Zhang Chen <18741306@qq.com>
2020-02-11 14:48:30 +0800

    add chat2.py for line chat history

---
+++ chat2.py
@@ -0,0 +1,51 @@
+def read_file(filename):
+	lines = []
+	with open (filename,'r',encoding='utf-8-sig') as f:
+		for line in f:
+			lines.append(line.strip())
+	return lines
+
+def convert(lines):
+	person = None
+	allen_word_count = 0
+	allen_sticker_count = 0
+	a_image_count = 0
+	viki_word_count = 0
+	viki_sticker_count = 0
+	v_image_count = 0
+	for line in lines:
+		s = line.split(' ')
+		time = s[0]
+		name = s[1]
+		if name == 'Allen':
+			if s[2] == '貼圖':
+				allen_sticker_count += 1
+			elif s[2] == '圖片':
+				a_image_count += 1
+			else:
+				for m in s[2:]:
+					allen_word_count += len(m) 
+		elif name == 'Viki':
+			if s[2] == '貼圖':
+				viki_sticker_count += 1
+			elif s[2] == '圖片':
+				v_image_count += 1
+			else:
+				for m in s[2:]:
+					viki_word_count += len(m)
+	print('allen说了',allen_word_count, '传了贴图',allen_sticker_count,'图片',a_image_count)
+	print('viki说了',viki_word_count,'传了贴图',viki_sticker_count,'图片',v_image_count)
+		#print(s)
+	
+
+def write_file(filename, lines):
+	with open(filename, 'w') as f:
+		for line in lines:
+			f.write(line + '\n')
+
+def main():
+	lines = read_file('LINE-Viki.txt')
+	lines = convert(lines)
+	#write_file('convert.txt',lines)
+
+main() #进入程序
